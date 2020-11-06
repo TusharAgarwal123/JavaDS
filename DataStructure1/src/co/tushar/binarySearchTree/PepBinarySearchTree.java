0cca0abb4fe778851c099afd7c694920618fdfd4
Tushar Agarwal <agarwaltushar2016@gmail.com>
2020-11-06 05:39:15 -0800

    first commit

---
+++ DataStructure1/src/co/tushar/binarySearchTree/PepBinarySearchTree.java
@@ -0,0 +1,23 @@
+package co.tushar.binarySearchTree;
+
+public class PepBinarySearchTree {
+
+	static class Node {
+		int val;
+		Node left;
+		Node right;
+
+		Node(int data) {
+			val = data;
+			left = null;
+			right = null;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
